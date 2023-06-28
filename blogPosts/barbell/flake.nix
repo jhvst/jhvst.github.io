bc89b7c53a3de6e55dfa100b935999da06ce2015
Juuso Haavisto <juuso@ponkila.com>
2023-06-28 21:14:13 +0100

    barbell: add more compile platforms

--- blogPosts/barbell/flake.nix
+++ blogPosts/barbell/flake.nix
@@ -2,52 +2,67 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    flake-root.url = "github:srid/flake-root";
     blog.url = "github:jhvst/jhvst.github.io";
     barbell.url = "github:jhvst/barbell";
   };
 
-  outputs = inputs@{ self, nixpkgs, ... }: {
+  outputs =
+    inputs@{ self
+    , nixpkgs
+    , flake-parts
+    , ...
+    }:
 
-    packages.aarch64-darwin.barbell = (
-      let
-        system = "aarch64-darwin";
-        pkgs = nixpkgs.legacyPackages.${system};
-        blog = inputs.blog.packages.${system};
-      in
-      pkgs.stdenv.mkDerivation {
-        name = "barbell";
-        src = ./.;
-        buildInputs = with pkgs; [
-          pandoc
-          validator-nu
-          nodePackages.js-beautify
-          inputs.barbell.packages.${system}.barbell
-          woff2
-          vale
-        ];
-        phases = [ "unpackPhase" "buildPhase" "checkPhase" ];
-        buildPhase = ''
-          mkdir -p $out/css
-          mkdir html
-          cp -r ${blog.components}/html/* ./html
-          cp ${pkgs.ibm-plex}/share/fonts/opentype/IBMPlexMono-Regular.otf .
-          woff2_compress IBMPlexMono-Regular.otf
-          cp IBMPlexMono-Regular.woff2 $out/
-          pandoc main.md --katex -o main.html
-          barbell main.html > article.bar
-          barbell ./html/template_article.html > $out/barbell.html
-        '';
+    flake-parts.lib.mkFlake { inherit inputs; } {
 
-        doCheck = false;
-        checkPhase = ''
-          vnu $out/barbell.html
-          vale sync
-          vale main.md
-        '';
-      }
-    );
+      systems = [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
+      imports = [
+        inputs.flake-root.flakeModule
+      ];
 
-    packages.aarch64-darwin.default = self.packages.aarch64-darwin.barbell;
+      perSystem = { pkgs, lib, config, system, ... }: rec {
 
-  };
+        packages.barbell = pkgs.stdenv.mkDerivation {
+          name = "barbell";
+          src = ./.;
+          buildInputs = with pkgs; [
+            pandoc
+            validator-nu
+            nodePackages.js-beautify
+            inputs.barbell.packages.${system}.barbell
+            woff2
+            vale
+          ];
+          phases = [ "unpackPhase" "buildPhase" "checkPhase" ];
+          buildPhase = ''
+            mkdir -p $out/css
+            mkdir html
+            cp -r ${inputs.blog.packages.${system}.components}/html/* ./html
+            cp ${pkgs.ibm-plex}/share/fonts/opentype/IBMPlexMono-Regular.otf .
+            woff2_compress IBMPlexMono-Regular.otf
+            cp IBMPlexMono-Regular.woff2 $out/
+            pandoc main.md --katex -o main.html
+            barbell main.html > article.bar
+            barbell ./html/template_article.html > $out/barbell.html
+          '';
+
+          doCheck = false;
+          checkPhase = ''
+            vnu $out/barbell.html
+            vale sync
+            vale main.md
+          '';
+        };
+
+        packages.default = packages.barbell;
+
+      };
+    };
 }
