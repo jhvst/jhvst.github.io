7cb60e6824aabd0513254b2f3efa3882298b633c
Juuso Haavisto <juuso@ponkila.com>
2023-06-28 16:27:16 +0100

    j1: add more build platforms

--- blogPosts/j1/flake.nix
+++ blogPosts/j1/flake.nix
@@ -1,53 +1,67 @@
 {
-  description = "A very basic flake";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    flake-root.url = "github:srid/flake-root";
     blog.url = "github:jhvst/jhvst.github.io";
     barbell.url = "github:jhvst/barbell";
   };
 
-  outputs = inputs@{ self, nixpkgs, ... }: {
+  outputs =
+    inputs@{ self
+    , nixpkgs
+    , flake-parts
+    , ...
+    }:
 
-    packages.aarch64-darwin.j1 = (
-      let
-        system = "aarch64-darwin";
-        pkgs = nixpkgs.legacyPackages.${system};
-        blog = inputs.blog.packages.${system};
-      in
-      pkgs.stdenv.mkDerivation {
-        name = "j1";
-        src = ./.;
-        buildInputs = with pkgs; [
-          pandoc
-          cbqn
-          validator-nu
-          nodePackages.js-beautify
-          inputs.barbell.packages.${system}.barbell
-        ];
-        phases = [ "unpackPhase" "buildPhase" "checkPhase" ];
-        buildPhase = ''
-          mkdir -p $out
-          mkdir -p $out/css
-          mkdir -p $out/img
-          mkdir -p $out/attachments
-          cp -r ${blog.components}/* .
-          pandoc j1.md -o article.bar
-          barbell template.html > $out/j1.html
-          js-beautify -f $out/j1.html -r
-          cp css/* $out/css
-          cp img/* $out/img
-          cp attachments/* $out/attachments
-        '';
+    flake-parts.lib.mkFlake { inherit inputs; } {
 
-        doCheck = true;
-        checkPhase = ''
-          vnu $out/j1.html
-        '';
-      }
-    );
+      systems = [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
+      imports = [
+        inputs.flake-root.flakeModule
+      ];
 
-    packages.aarch64-darwin.default = self.packages.aarch64-darwin.j1;
+      perSystem = { pkgs, lib, config, system, ... }: rec {
 
-  };
+        packages.j1 = pkgs.stdenv.mkDerivation {
+          name = "j1";
+          src = ./.;
+          buildInputs = with pkgs; [
+            pandoc
+            cbqn
+            validator-nu
+            nodePackages.js-beautify
+            inputs.barbell.packages.${system}.barbell
+          ];
+          phases = [ "unpackPhase" "buildPhase" "checkPhase" ];
+          buildPhase = ''
+            mkdir -p $out
+            mkdir -p $out/css
+            mkdir -p $out/img
+            mkdir -p $out/attachments
+            cp -r ${inputs.blog.packages.${system}.components}/* .
+            pandoc j1.md -o article.bar
+            barbell template.html > $out/j1.html
+            js-beautify -f $out/j1.html -r
+            cp css/* $out/css
+            cp img/* $out/img
+            cp attachments/* $out/attachments
+          '';
+
+          doCheck = true;
+          checkPhase = ''
+            vnu $out/j1.html
+          '';
+        };
+
+        packages.default = packages.j1;
+
+      };
+    };
 }
