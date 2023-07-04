7d1705e032cf685195bba93e87a96b9e480dac9e
Juuso Haavisto <juuso@ponkila.com>
2023-07-04 18:47:09 +0100

    bsc-thesis: init

---
+++ papers/bsc-thesis/flake.nix
@@ -0,0 +1,48 @@
+{
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    flake-root.url = "github:srid/flake-root";
+  };
+
+  outputs =
+    inputs@{ self
+    , nixpkgs
+    , flake-parts
+    , ...
+    }:
+
+    flake-parts.lib.mkFlake { inherit inputs; } {
+
+      systems = [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
+      imports = [
+        inputs.flake-root.flakeModule
+      ];
+
+      perSystem = { pkgs, lib, config, system, ... }: rec {
+
+        packages.bsc-thesis = pkgs.stdenv.mkDerivation {
+
+          name = "bsc-thesis";
+          src = ./.;
+          buildInputs = with pkgs; [
+            pandoc
+          ];
+          phases = [ "unpackPhase" "buildPhase" ];
+          buildPhase = ''
+            mkdir $out
+            pandoc main.tex -sC --toc --bibliography=refs.bib > $out/bsc-thesis.html
+          '';
+        };
+
+        packages.default = packages.bsc-thesis;
+
+      };
+    };
+}
