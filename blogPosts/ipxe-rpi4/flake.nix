47423c33d4c2ca03ee13070125e31a8b96410e85
Juuso Haavisto <juuso@ponkila.com>
2023-07-25 22:12:57 +0100

    blogPosts: ipxe-rpi4 init

---
+++ blogPosts/ipxe-rpi4/flake.nix
@@ -0,0 +1,77 @@
+{
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    flake-root.url = "github:srid/flake-root";
+    blog.url = "github:jhvst/jhvst.github.io";
+    barbell.url = "github:jhvst/barbell";
+  };
+
+  outputs =
+    inputs@{ self
+    , nixpkgs
+    , flake-parts
+    , ...
+    }:
+
+    flake-parts.lib.mkFlake { inherit inputs; } {
+
+      systems = [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
+      imports = [
+        inputs.flake-root.flakeModule
+      ];
+
+      perSystem = { pkgs, lib, config, system, ... }: rec {
+
+        packages.default = pkgs.stdenv.mkDerivation rec {
+
+          title = "iPXE, NixOS, EDK2 UEFI BIOS, and Raspberry Pi 4";
+          description = "This is an excerpt from a reddit comment of mine in response to running NixOS with additional kernel modules on NixOS.";
+          pubDate = "12 Nov 2022 11:57:50 GMT";
+
+          name = "ipxe-rpi4";
+          src = ./.;
+          buildInputs = with pkgs; [
+            inputs.barbell.packages.${system}.barbell
+            nodePackages.js-beautify
+            pandoc
+            python311Packages.python-slugify
+            validator-nu
+            woff2
+          ];
+          phases = [ "unpackPhase" "buildPhase" "checkPhase" ];
+          buildPhase = ''
+            mkdir -p $out/css
+            mkdir -p $out/img
+            mkdir html
+            cp -r ${inputs.blog.packages.${system}.components}/html/* ./html
+            cp ${pkgs.ibm-plex}/share/fonts/opentype/IBMPlexMono-Regular.otf .
+            woff2_compress IBMPlexMono-Regular.otf
+            cp IBMPlexMono-Regular.woff2 $out/
+            pandoc main.md --katex -o main.html
+            echo "${title}" > title.bar
+            echo "${description}" > description.bar
+            echo "${pubDate}" > pubDate.bar
+            echo "${name}" > name.bar
+            slugify ${title} > slug.bar
+            date -d "${pubDate}" -Iminutes > datetime.bar
+            cat main.md | wc -w > wordCount.bar
+            barbell main.html > article.bar
+            barbell ./html/template_article.html > $out/$(slugify ${title}).html
+            js-beautify -f $out/$(slugify ${title}).html -r
+          '';
+
+          doCheck = true;
+          checkPhase = ''
+            vnu $out/$(slugify ${title}).html
+          '';
+        };
+      };
+    };
+}
