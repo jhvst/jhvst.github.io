483284f2ba3d64108a455a54964bc857b02d7952
Juuso Haavisto <juuso@ponkila.com>
2023-09-02 00:20:21 +0100

    vksum: update to web-components

--- blogPosts/vulkan-sum-reduction/flake.nix
+++ blogPosts/vulkan-sum-reduction/flake.nix
@@ -2,36 +2,26 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs";
-    flake-parts.url = "github:hercules-ci/flake-parts";
-    flake-root.url = "github:srid/flake-root";
-    blog.url = "github:jhvst/jhvst.github.io";
+    web-components.url = "github:jhvst/jhvst.github.io?dir=packages/web-components";
     barbell.url = "github:jhvst/barbell";
   };
 
-  outputs =
-    inputs@{ self
-    , nixpkgs
-    , flake-parts
-    , ...
-    }:
+  outputs = { self, nixpkgs, ... } @inputs:
 
-    flake-parts.lib.mkFlake { inherit inputs; } {
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+        inherit system;
+        pkgs = import nixpkgs { inherit system; };
+      });
+    in
 
-      systems = [
-        "aarch64-darwin"
-        "aarch64-linux"
-        "x86_64-darwin"
-        "x86_64-linux"
-      ];
-      imports = [
-        inputs.flake-root.flakeModule
-      ];
-
-      perSystem = { pkgs, lib, config, system, ... }: rec {
-
-        packages.default = pkgs.stdenv.mkDerivation rec {
+    {
+      packages = forEachSupportedSystem ({ pkgs, system }: {
+        default = pkgs.stdenv.mkDerivation rec {
 
           title = "Vulkan sum reduction";
+          description = "We describe how we can do sum reduction using SPIR-V subgroups up to vector length of 4096. With SPIR-V, we are able to run this program on GPUs of different manufacturers using the Vulkan GPU API. As Vulkan is supported by Nvidia, AMD, and, e.g., Apple ARM among others, this allows us to build cross-compatible array operations similar to CUDA.";
           pubDate = "23 Mar 2022 16:30:00 GMT";
 
           name = "vksum";
@@ -41,7 +31,6 @@
             nodePackages.js-beautify
             pandoc
             python311Packages.python-slugify
-            vale
             validator-nu
             woff2
           ];
@@ -50,27 +39,31 @@
             mkdir -p $out/css
             mkdir -p $out/img
             mkdir html
-            cp -r ${inputs.blog.packages.${system}.components}/html/* ./html
+            cp -r ${inputs.web-components.packages.${system}.default}/html/* ./html
             cp ${pkgs.ibm-plex}/share/fonts/opentype/IBMPlexMono-Regular.otf .
             cp -r img/* $out/img
             woff2_compress IBMPlexMono-Regular.otf
             cp IBMPlexMono-Regular.woff2 $out/
             pandoc main.md --katex -o main.html
+
             echo "${title}" > title.bar
+            echo "${description}" > description.bar
             echo "${pubDate}" > pubDate.bar
+            echo "${name}" > name.bar
+            slugify ${title} > slug.bar
             date -d "${pubDate}" -Iminutes > datetime.bar
+            cat main.md | wc -w > wordCount.bar
+
             barbell main.html > article.bar
             barbell ./html/template_article.html > $out/$(slugify ${title}).html
             js-beautify -f $out/$(slugify ${title}).html -r
           '';
 
-          doCheck = false;
+          doCheck = true;
           checkPhase = ''
             vnu $out/$(slugify ${title}).html
-            vale sync
-            vale main.md
           '';
         };
-      };
+      });
     };
 }
