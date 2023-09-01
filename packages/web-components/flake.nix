2b22d8b347041d5ea193b95a685fc337a64f18e9
Juuso Haavisto <juuso@ponkila.com>
2023-09-01 23:57:47 +0100

    packages/web-components: init

---
+++ packages/web-components/flake.nix
@@ -0,0 +1,33 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, ... } @ inputs:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+        inherit system;
+        pkgs = import nixpkgs { inherit system; };
+      });
+    in
+    {
+      packages = forEachSupportedSystem ({ pkgs, system }: {
+
+        default = pkgs.stdenv.mkDerivation {
+
+          name = "components";
+          src = ./../..;
+
+          phases = [ "unpackPhase" "buildPhase" ];
+
+          buildPhase = ''
+            mkdir -p $out/css
+            mkdir -p $out/html
+            cp css/* $out/css
+            cp html/* $out/html
+          '';
+        };
+      });
+    };
+}
