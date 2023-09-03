5a821954c14c451e0d2a69a4cadfa3c76445e6f0
Juuso Haavisto <juuso@ponkila.com>
2023-09-03 20:33:42 +0100

    templates: update default template

--- templates/flake.nix
+++ templates/flake.nix
@@ -1,11 +1,10 @@
 {
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs";
-    flake-parts.url = "github:hercules-ci/flake-parts";
-    flake-root.url = "github:srid/flake-root";
-    blog-html.url = "github:jhvst/jhvst.github.io?dir=html";
     barbell.url = "github:jhvst/barbell";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:nixos/nixpkgs";
+    web-components.url = "github:jhvst/jhvst.github.io?dir=packages/web-components";
   };
 
   outputs =
@@ -17,17 +16,10 @@
 
     flake-parts.lib.mkFlake { inherit inputs; } {
 
-      systems = [
-        "aarch64-darwin"
-        "aarch64-linux"
-        "x86_64-darwin"
-        "x86_64-linux"
-      ];
-      imports = [
-        inputs.flake-root.flakeModule
-      ];
+      systems = nixpkgs.lib.systems.flakeExposed;
+      imports = [ ];
 
-      perSystem = { pkgs, lib, config, system, ... }: rec {
+      perSystem = { pkgs, lib, config, inputs', ... }: {
 
         packages.default = pkgs.stdenv.mkDerivation rec {
 
@@ -38,7 +30,7 @@
           name = "";
           src = ./.;
           buildInputs = with pkgs; [
-            inputs.barbell.packages.${system}.barbell
+            inputs'.barbell.packages.barbell
             nodePackages.js-beautify
             pandoc
             python311Packages.python-slugify
@@ -50,11 +42,13 @@
             mkdir -p $out/css
             mkdir -p $out/img
             mkdir html
-            cp -r ${inputs.blog-html.packages.${system}.default}/* ./html
+            cp -r ${inputs'.web-components.packages.default}/html/* ./html
             cp ${pkgs.ibm-plex}/share/fonts/opentype/IBMPlexMono-Regular.otf .
+            cp -r img/* $out/img
             woff2_compress IBMPlexMono-Regular.otf
             cp IBMPlexMono-Regular.woff2 $out/
             pandoc main.md --katex -o main.html
+
             echo "${title}" > title.bar
             echo "${description}" > description.bar
             echo "${pubDate}" > pubDate.bar
@@ -62,6 +56,7 @@
             slugify ${title} > slug.bar
             date -d "${pubDate}" -Iminutes > datetime.bar
             cat main.md | wc -w > wordCount.bar
+
             barbell main.html > article.bar
             barbell html/template_article.html > $out/$(slugify ${title}).html
             js-beautify -f $out/$(slugify ${title}).html -r
