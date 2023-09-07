000f02db73db057c246656757fcd9d6ea9d8196a
Juuso Haavisto <juuso@ponkila.com>
2023-09-07 20:28:25 +0100

    flake: separate neovim into its own package

--- flake.nix
+++ flake.nix
@@ -3,15 +3,10 @@
   inputs = {
     devenv.url = "github:cachix/devenv";
     flake-parts.url = "github:hercules-ci/flake-parts";
-    juuso.inputs.nixpkgs.follows = "nixpkgs";
-    juuso.url = "github:jhvst/nix-config";
     mk-shell-bin.url = "github:rrbutani/nix-mk-shell-bin";
     nix2container.inputs.nixpkgs.follows = "nixpkgs";
     nix2container.url = "github:nlewo/nix2container";
-    nixneovimplugins.url = "github:jooooscha/nixpkgs-vim-extra-plugins";
     nixpkgs.url = "github:nixos/nixpkgs";
-    nixvim.inputs.nixpkgs.follows = "nixpkgs";
-    nixvim.url = "github:nix-community/nixvim";
 
     # blogposts
     j1.url = "github:jhvst/jhvst.github.io?dir=blogPosts/j1";
@@ -29,9 +24,7 @@
   outputs =
     inputs@{ self
     , flake-parts
-    , nixneovimplugins
     , nixpkgs
-    , nixvim
     , ...
     }:
 
@@ -49,13 +42,6 @@
 
       perSystem = { pkgs, lib, config, system, ... }: {
 
-        _module.args.pkgs = import inputs.nixpkgs {
-          inherit system;
-          overlays = [
-            inputs.juuso.overlays.default
-          ];
-          config = { };
-        };
 
         packages.default = pkgs.stdenv.mkDerivation {
 
@@ -107,67 +93,7 @@
 
         };
 
-        packages.neovim = nixvim.legacyPackages.${system}.makeNixvimWithModule {
-          inherit pkgs;
-          module = {
-            imports = [
-              inputs.juuso.outputs.nixosModules.neovim
-            ];
-            extraConfigVim = ''
-              let g:sqlite_clib_path = '${pkgs.sqlite.out}/lib/libsqlite3.dylib'
-
-              let g:limelight_bop = '^'
-              let g:limelight_eop = '$'
-
-              augroup autoformat_settings
-                autocmd FileType html,css,sass,scss,less,json,js AutoFormatBuffer js-beautify
-              augroup END
-            '';
-            extraConfigLua = ''
-              require("papis").setup({
-                db_path = "/Users/juuso/.papis/papis-nvim.sqlite3",
-                papis_python = {
-                  dir = "/Users/juuso/.papis",
-                  info_name = "info.yaml",
-                  notes_name = [[notes.org]],
-                },
-                enable_keymaps = true,
-              })
-            '';
-            extraPackages = with pkgs; [
-              nodePackages.js-beautify
-              papis
-              sqlite
-              yq-go
-              # ncurses # papis has dependency on ncurses, but it is broken on macOS -- install with brew instead. see: https://github.com/jhvst/nix-config/commit/360220836e1f03b5b0668f2f33af1ecc247d8d15
-            ];
-            extraPlugins = with pkgs.vimPlugins; [
-              goyo-vim
-              limelight-vim # :LimeLight (also, consider :setlocal spell spelllang=en_us
-              markdown-preview-nvim # :MarkdownPreview
-              nui-nvim
-              null-ls-nvim
-              plenary-nvim
-            ] ++ [
-              nixneovimplugins.packages.${system}.papis-nvim
-              nixneovimplugins.packages.${system}.sqlite-lua
-            ];
-            plugins.lsp.servers.html.enable = true;
-            plugins.nvim-cmp = {
-              sources =
-                [
-                  { name = "latex_symbols"; }
-                  { name = "papis"; }
-                ];
-            };
-          };
-        };
-
         devenv.shells.default = {
-
-          packages = [
-            config.packages.neovim
-          ];
 
           scripts = {
             img-compress = {
---
+++ packages/neovim/flake.nix
@@ -0,0 +1,103 @@
+{
+
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    juuso.inputs.nixpkgs.follows = "nixpkgs";
+    juuso.url = "github:jhvst/nix-config";
+    nixneovimplugins.url = "github:jooooscha/nixpkgs-vim-extra-plugins";
+    nixpkgs.url = "github:nixos/nixpkgs";
+    nixvim.inputs.nixpkgs.follows = "nixpkgs";
+    nixvim.url = "github:nix-community/nixvim";
+  };
+
+  outputs =
+    inputs@{ self
+    , flake-parts
+    , nixneovimplugins
+    , nixpkgs
+    , nixvim
+    , ...
+    }:
+
+    flake-parts.lib.mkFlake { inherit inputs; } {
+
+      systems = [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
+      imports = [ ];
+
+      perSystem = { pkgs, lib, config, system, ... }: {
+
+        _module.args.pkgs = import inputs.nixpkgs {
+          inherit system;
+          overlays = [
+            inputs.juuso.overlays.default
+          ];
+          config = { };
+        };
+
+        packages.neovim = nixvim.legacyPackages.${system}.makeNixvimWithModule {
+          inherit pkgs;
+          module = {
+            imports = [
+              inputs.juuso.outputs.nixosModules.neovim
+            ];
+            extraConfigVim = ''
+              let g:sqlite_clib_path = '${pkgs.sqlite.out}/lib/libsqlite3.dylib'
+
+              let g:limelight_bop = '^'
+              let g:limelight_eop = '$'
+
+              augroup autoformat_settings
+                autocmd FileType html,css,sass,scss,less,json,js AutoFormatBuffer js-beautify
+              augroup END
+            '';
+            extraConfigLua = ''
+              require("papis").setup({
+                db_path = "/Users/juuso/.papis/papis-nvim.sqlite3",
+                papis_python = {
+                  dir = "/Users/juuso/.papis",
+                  info_name = "info.yaml",
+                  notes_name = [[notes.org]],
+                },
+                enable_keymaps = true,
+              })
+            '';
+            extraPackages = with pkgs; [
+              nodePackages.js-beautify
+              papis
+              sqlite
+              yq-go
+              # ncurses # papis has dependency on ncurses, but it is broken on macOS -- install with brew instead. see: https://github.com/jhvst/nix-config/commit/360220836e1f03b5b0668f2f33af1ecc247d8d15
+            ];
+            extraPlugins = with pkgs.vimPlugins; [
+              goyo-vim
+              limelight-vim # :LimeLight (also, consider :setlocal spell spelllang=en_us
+              markdown-preview-nvim # :MarkdownPreview
+              nui-nvim
+              null-ls-nvim
+              plenary-nvim
+            ] ++ [
+              nixneovimplugins.packages.${system}.papis-nvim
+              nixneovimplugins.packages.${system}.sqlite-lua
+            ];
+            plugins.lsp.servers.html.enable = true;
+            plugins.nvim-cmp = {
+              sources =
+                [
+                  { name = "latex_symbols"; }
+                  { name = "papis"; }
+                ];
+            };
+          };
+        };
+
+        packages.default = config.neovim;
+
+      };
+
+    };
+}
